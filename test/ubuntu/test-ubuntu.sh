a1f6d0594cc675cf67fafc80cae2778a3501f0e9
Dave Kerr <dwmkerr@gmail.com>
2018-01-09 17:07:25 +0700

    less verbose, fixed paths

--- test/ubuntu/test-ubuntu.sh
+++ test/ubuntu/test-ubuntu.sh
@@ -7,8 +7,7 @@ sudo apt remove --purge imagemagick
 # Install IM 6.
 version="ImageMagick-6.9.9-33"
 wget http://www.imagemagick.org/download/${version}.tar.gz
-wget http://www.imagemagick.org/download/${version}.tar.gz
-tar xvzf ${version}.tar.gz
+tar xzf ${version}.tar.gz
 cd ${version}
 touch configure
 ./configure
@@ -19,13 +18,12 @@ convert -version
 cd ..
 
 # Check the ImageMagick version with node-imagemagick-cli.
-CHECK_VERSION=6 node ../../test/check-imagemagick-version.js
+CHECK_VERSION=6 node ./test/check-imagemagick-version.js
 
 # Install IM 7.
 version="ImageMagick-7.0.7-21"
 wget http://www.imagemagick.org/download/${version}.tar.gz
-wget http://www.imagemagick.org/download/${version}.tar.gz
-tar xvzf ${version}.tar.gz
+tar xzf ${version}.tar.gz
 cd ${version}
 touch configure
 ./configure
@@ -36,4 +34,4 @@ convert -version
 cd ..
 
 # Check the ImageMagick version with node-imagemagick-cli.
-CHECK_VERSION=7 node ../../test/check-imagemagick-version.js
+CHECK_VERSION=7 node ./test/check-imagemagick-version.js
