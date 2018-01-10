26d94eefd8d9b69abb62493858c24216b9cc32aa
Dave Kerr <dwmkerr@gmail.com>
2018-01-10 12:03:47 +0700

    fix error handling on windows tests

--- test/windows/test-windows.ps1
+++ test/windows/test-windows.ps1
@@ -1,6 +1,10 @@
+# Ensure that if there is any failure, we bail and abort.
+$script:ErrorActionPreference = 'Stop'
+
 # Install IM 6.
-choco install imagemagick.tool --version "6.9.9.23"
+choco install imagemagick.tool --version "6.9.9.23" --no-progress
 refreshenv
 
 # Test for IM 6.
-CHECK_VERSION=9 node .\\test\\check-imagemagick-version.js
+$env:CHECK_VERSION="9"
+node .\\test\\check-imagemagick-version.js
