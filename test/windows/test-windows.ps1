99eac25835e21950f0f94b2b00a264f30af55bfd
Dave Kerr <dwmkerr@gmail.com>
2018-01-10 11:48:20 +0700

    update test scripts

--- test/windows/test-windows.ps1
+++ test/windows/test-windows.ps1
@@ -1,5 +1,6 @@
 # Install IM 6.
-choco install imagemagick.tool --version "6.9.9.23" && refreshenv
+choco install imagemagick.tool --version "6.9.9.23"
+refreshenv
 
 # Test for IM 6.
 CHECK_VERSION=9 node .\\test\\check-imagemagick-version.js
