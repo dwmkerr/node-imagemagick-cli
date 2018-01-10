8c1a49640a15a04e615eff1044a7332ffb36e33b
Dave Kerr <dwmkerr@gmail.com>
2018-01-10 12:19:43 +0700

    logging for the pick cli

--- src/pick-cli.js
+++ src/pick-cli.js
@@ -16,12 +16,17 @@ function pickCli(cli) {
     //  We're on windows, so we need to see the available options with 'where'.
     return childProcess.exec(`where ${cli}`, (err, stdout) => {
       if (err) return reject(err);
+
+      console.log(`where has returned: ${stdout}`);
+
       const options = stdout.split('\n');
 
       //  The first regex to pass wins.
       for (let i = 0; i < regexes.length; i += 1) {
         for (let j = 0; j < options.length; j += 1) {
-          if (regexes[i].test(options[j])) return resolve(options[j]);
+          const result = regexes[i].test(options[j]);
+          console.log(`testing with '${regexes[i]}' with '${options[j]}' returned: ${result}`);
+          if (result) return resolve(options[j]);
         }
       }
 
