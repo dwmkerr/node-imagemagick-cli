55c111f7cad89afe4e21d5cdd51e2e6f3becb3d4
Dave Kerr <dwmkerr@gmail.com>
2018-01-10 14:52:50 +0700

    test v7

--- test/windows/test-windows.ps1
+++ test/windows/test-windows.ps1
@@ -6,7 +6,22 @@ choco install imagemagick.tool --version "6.9.9.23" --no-progress
 refreshenv
 
 # Test for IM 6.
-$env:CHECK_VERSION="9"
+$env:CHECK_VERSION="6"
+node .\\test\\check-imagemagick-version.js
+If ($LastExitCode -ne 0) {
+    Throw "ImageMagick Version Test Failed"
+}
+
+# Uninstall IM 6.
+choco uninstall imagemagick.tool --version "6.9.9.23" --no-progress
+refreshenv
+
+# Install IM 7.
+choco install imagemagick.tool --version "7.0.7.6" --no-progress
+refreshenv
+
+# Test for IM 7.
+$env:CHECK_VERSION="7"
 node .\\test\\check-imagemagick-version.js
 If ($LastExitCode -ne 0) {
     Throw "ImageMagick Version Test Failed"
