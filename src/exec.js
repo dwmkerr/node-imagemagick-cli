50b374875757659f68d68419871a94a916a40b8e
Dave Kerr <dwmkerr@gmail.com>
2020-01-15 00:31:55 +0800

    fix: getting tests working for IM 7.0.1 Q16

--- src/exec.js
+++ src/exec.js
@@ -10,72 +10,70 @@ const cliPathsMap = {
 
 async function mapCli(cli) {
   //  If we have already worked out the path to the CLI, great.
-  if (cliPathsMap[cli]) return Promise.resolve(cliPathsMap[cli]);
+  if (cliPathsMap[cli]) return cliPathsMap[cli];
 
   //  If we're not on Windows, we just use the cli as-is.
   if (!/^win/.test(process.platform)) {
     cliPathsMap[cli] = cli;
-    return Promise.resolve(cliPathsMap[cli]);
+    return cliPathsMap[cli];
   }
 
   //  On Windows we need to pick the right CLI, internally using the 'where'
   //  command.
-  let cliPath = await pickCli(cli)
-    .then((mappedCli) => {
-      cliPathsMap[cli] = mappedCli;
-      return mappedCli;
-    });
+  const cliPath = await pickCli(cli);
 
-  // On windows, path should contain "ImageMagick/"
-  if (/^win/.test(process.platform)) {
-    if (!/ImageMagick\//.test(cliPath)) {
-      // Try again searching for magick.exe
-      cliPath = await pickCli('magick')
-        .then((mappedCli) => {
-          cliPathsMap[cli] = mappedCli;
-          return mappedCli;
-        });
-      if (!cliPath) {
-        throw new Error('Could not find magick.exe. Is it installed in the default "ImageMagick" folder?');
-      }
-    }
+  //  If we have a found a path in the "ImageMagick" folder, we're good.
+  if (/ImageMagick\//.test(cliPath)) {
+    cliPathsMap[cli] = cliPath;
+    return cliPath;
   }
 
-  return cliPath;
+  //  If we have *not* found a path, it is probably because we are calling
+  //  a command like 'convert.exe' directly, rather than 'magick.exe convert'
+  //  and using IM 7.0.1-Q16. So instead, search for the 'magick' CLI and use
+  //  that.
+  const magickCliPath = await pickCli('magick');
+  if (!magickCliPath) {
+    throw new Error('Could not find magick.exe. Is it installed in the default "ImageMagick" folder?');
+  }
+  cliPathsMap[cli] = magickCliPath;
+  return magickCliPath;
 }
 
-function exec(command) {
+async function exec(command) {
+  //  First, extract the cli and parameters.
+  const { cli, parameters } = deconstructCommand(command);
+
+  //  Map the cli to a path.
+  const mappedCli = await mapCli(cli);
+
+  // Is this version 7.0.1-Q16?
+  const isV701Q16 = !mappedCli.includes(cli);
+
+  //  We have the CLI path mapped, which means we can reconstruct the command
+  //  with the appropriate path and execute it.
+  const reconstructedCommand = `"${mappedCli}" ${isV701Q16 ? cli : ''} ${parameters}`;
+  debug(`Preparing to execute: ${reconstructedCommand}`);
+
   return new Promise((resolve, reject) => {
-    //  First, extract the cli and parameters.
-    const { cli, parameters } = deconstructCommand(command);
+    try {
+      return childProcess.exec(reconstructedCommand, (err, stdout, stderr) => {
+        debug(`  err: ${err ? err.toString() : '<null>'}`);
+        debug(`  stdout: ${stdout}`);
+        debug(`  stderr: ${stderr}`);
+        if (!stdout && err) {
+          const errorMessage = `Failed to call '${command}', which was mapped to '${reconstructedCommand}'. Error is '${err.message}'.`;
+          const error = new Error(errorMessage);
+          error.stdout = stdout;
+          error.stderr = stderr;
+          return reject(error);
+        }
 
-    //  Map the cli to a path.
-    mapCli(cli)
-      .then((mappedCli) => {
-        // Is this version 7.0.1-Q16?
-        const isV701Q16 = !mappedCli.includes(cli);
-
-        //  We have the CLI path mapped, which means we can reconstruct the command
-        //  with the appropriate path and execute it.
-        const reconstructedCommand = `"${mappedCli}" ${isV701Q16 ? cli : ''} ${parameters}`;
-        debug(`Preparing to execute: ${reconstructedCommand}`);
-
-        childProcess.exec(reconstructedCommand, (err, stdout, stderr) => {
-          debug(`  err: ${err ? err.toString() : '<null>'}`);
-          debug(`  stdout: ${stdout}`);
-          debug(`  stderr: ${stderr}`);
-          if (!stdout && err) {
-            const errorMessage = `Failed to call '${command}', which was mapped to '${reconstructedCommand}'. Error is '${err.message}'.`;
-            const error = new Error(errorMessage);
-            error.stdout = stdout;
-            error.stderr = stderr;
-            return reject(error);
-          }
-
-          return resolve({ stdout, stderr });
-        });
-      })
-      .catch(reject);
+        return resolve({ stdout, stderr });
+      });
+    } catch (err) {
+      return reject(err);
+    }
   });
 }
 
--- src/exec.spec.js
+++ src/exec.spec.js
@@ -3,13 +3,12 @@ const sinon = require('sinon'); const childProcess = require('child_process');
 const exec = require('./exec');
 
 describe('exec', () => {
-  let sandbox = sinon.createSandbox();
+  const sandbox = sinon.createSandbox();
   let platform;
 
   beforeEach(() => {
     //  Save the platform, we'll mess with it in tests.
     platform = Object.getOwnPropertyDescriptor(process, 'platform');
-    sandbox = sinon.createSandbox();
   });
 
   afterEach(() => {
@@ -17,103 +16,104 @@ describe('exec', () => {
     sandbox.restore();
   });
 
-  it('should fail if the command is not in the form \'cmd\' \'parameters\'', () => {
-    return exec('command-only')
-      .then(() => {
-        throw new Error('This call should not succeed.');
-      })
-      .catch((err) => {
-        expect(err.message).to.match(/cli*.parameters/);
-      });
+  it('should fail if the command is not in the form \'cmd\' \'parameters\'', async () => {
+    try {
+      await exec('command-only');
+      throw new Error('This call should not succeed.');
+    } catch (err) {
+      expect(err.message).to.match(/cli*.parameters/);
+    }
   });
 
-  it('should return a sensible error if the command does not exist', () => {
+  it('should return a sensible error if the command does not exist', async () => {
     //  Fake the OSX platform, otherwise on Windows we will try and use
     //  'where' to identify the cli.
     Object.defineProperty(process, 'platform', { value: 'darwin' });
 
     //  We expect 'childProcess.exec' to be called with 'which'.
     sandbox.stub(childProcess, 'exec')
-      .callsFake((cmd, callback) => {
+      .callsFake((_, callback) => {
         const err = new Error('command: no-way does not exist');
         callback(err, null, 'command: no-way does not exist');
       });
 
-    return exec('no-way parameters')
-      .then(() => {
-        throw new Error('This command should fail.');
-      })
-      .catch((err) => {
-        expect(err.message).to.match(/does not exist/);
-        expect(err.stderr).to.match(/does not exist/);
-      });
+    try {
+      await exec('no-way parameters');
+      throw new Error('This command should fail.');
+    } catch (err) {
+      expect(err.message).to.match(/does not exist/);
+      expect(err.stderr).to.match(/does not exist/);
+    }
   });
 
-  it('should prefer a command in an \'ImageMagick\' folder on windows', () => {
-    //  Fake the win32 platform.
-    Object.defineProperty(process, 'platform', { value: 'win32' });
-
-    // //  We expect 'childProcess.exec' to be called with 'which'.
-    // sandbox.stub(childProcess, 'exec')
-    //   .withArgs('where convert')
-    //   .callsFake((cmd, callback) => {
-    //     //  Give a set of paths, essentially mocking the 'convert' problem:
-    //     //  http://www.imagemagick.org/Usage/windows/#convert_issue
-    //     const paths = [
-    //       'c:\\System32\\convert.exe',
-    //       'c:\\ProgramFiles\\ImageMagick\\Convert.exe',
-    //     ];
-    //     callback(null, paths.join('\r\n'));
-    //   })
-    //   //  Notice that the path is quoted...
-    //   .withArgs('"c:\\ProgramFiles\\ImageMagick\\Convert.exe" -v')
-    //   .callsFake((cmd, callback) => {
-    //     callback(null, 'Version: ImageMagick');
-    //   });
-
-    //  We should prefer the ImageMagick path.
-    return exec('convert -v')
-      .then(({ stdout }) => {
-        expect(stdout).to.match(/Version: ImageMagick/);
-      })
-      .catch((err) => { throw new Error(`'${err}' should not be thrown.`); });
-  });
-
-  it('can pick a CLI when ImageMagick does not have convert.exe anymore (v7.0.1-Q16)', () => {
+  it('should prefer a command in an \'ImageMagick\' folder on windows', async () => {
     //  Fake the win32 platform.
     Object.defineProperty(process, 'platform', { value: 'win32' });
 
     //  We expect 'childProcess.exec' to be called with 'which'.
     sandbox.stub(childProcess, 'exec')
       .withArgs('where convert')
-      .callsFake((cmd, callback) => {
+      .callsFake((_, callback) => {
         //  Give a set of paths, essentially mocking the 'convert' problem:
-        //  https://stackoverflow.com/questions/41104358/imagemagick-command-line-usage
-        //  https://github.com/aheckmann/gm/issues/684
+        //  http://www.imagemagick.org/Usage/windows/#convert_issue
+        const paths = [
+          'c:\\System32\\convert.exe',
+          'c:\\ProgramFiles\\ImageMagick\\Convert.exe',
+        ];
+        callback(null, paths.join('\r\n'));
+      })
+      //  Notice that the path is quoted...
+      .withArgs('"c:\\ProgramFiles\\ImageMagick\\Convert.exe" -v')
+      .callsFake((_, callback) => {
+        callback(null, 'Version: ImageMagick');
+      });
+
+    //  We should prefer the ImageMagick path.
+    try {
+      const { stdout } = await exec('convert -v');
+      expect(stdout).to.match(/Version: ImageMagick/);
+    } catch (err) {
+      throw new Error(`'${err}' should not be thrown.`);
+    }
+  });
+
+  it('can pick a CLI when ImageMagick does not have convert.exe anymore (v7.0.1-Q16)', async () => {
+    //  Fake the win32 platform.
+    Object.defineProperty(process, 'platform', { value: 'win32' });
+
+    //  We expect 'childProcess.exec' to be called with 'which'.
+    sandbox.stub(childProcess, 'exec')
+      .withArgs('where convert')
+      .callsFake((_, callback) => {
+        // Mock the 'convert' problem, as well as the v7-Q16 problem; i.e.
+        // there is no 'convert' executable for IM (only the 'magick' executable
+        // will be available).
         const paths = [
           'c:\\System32\\convert.exe',
           '',
         ];
         callback(null, paths.join('\r\n'));
       })
+      // Now we expect that we'll look for 'magick' instead...
       .withArgs('where magick')
-      .callsFake((cmd, callback) => {
+      .callsFake((_, callback) => {
         const paths = [
+          // ...and it will live in the ImageMagick folder.
           'C:\\Program Files\\ImageMagick-7.0.9-Q16\\magick.exe',
         ];
         callback(null, paths.join('\r\n'));
       })
-      //  Notice that the path is quoted...
       .withArgs('"C:\\Program Files\\ImageMagick-7.0.9-Q16\\magick.exe" convert -v')
-      .callsFake((cmd, callback) => {
+      .callsFake((_, callback) => {
         callback(null, 'Version: ImageMagick');
       });
 
     //  We should prefer the ImageMagick path.
-    return exec('convert -v')
-      .then(({ stdout }) => {
-        expect(stdout).to.match(/Version: ImageMagick/);
-      })
-      .catch((err) => { throw new Error(`'${err}' should not be thrown.`); });
+    try {
+      const { stdout } = await exec('convert -v');
+      expect(stdout).to.match(/Version: ImageMagick/);
+    } catch (err) {
+      throw new Error(`'${err}' should not be thrown.`);
+    }
   });
 });
